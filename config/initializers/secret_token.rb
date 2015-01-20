ef43aaf476c03c9ff22a002941c8ce02d7df0c55
Lou Scheermann <louscheermann@lous-mbp.fritz.box>
2015-01-20 19:30:03 +0100

    Created a rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clubmood::Application.config.secret_token = '7f5d177873e18036d70a6c0f54720083bc5d2fd6809029eaedef150d2e00cf0c247cab3184e750bb9bdbbf9a4b6fb26e4814e0bac20c40ccc68be5fa362c3bc5'
